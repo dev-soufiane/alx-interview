eb28eb0f2481600bb8899e35aa0deba81199c862
dev-soufiane <dev.sofyane@gmail.com>
2024-07-19 01:00:48 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,12 +12,12 @@ def rotate_2d_matrix(matrix):
 
     """
     n = len(matrix)
-
-
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            
-    for i in range(n):
-        # Use slicing to reverse the row
-        matrix[i] = matrix[i][::-1]
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[y][x]
+            matrix[y][x] = matrix[j][y]
+            matrix[j][y] = tmp
