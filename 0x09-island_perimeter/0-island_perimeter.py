46ac0a9b9f3e9301e1f2104b0b68e256c36a2e1a
dev-soufiane <dev.sofyane@gmail.com>
2024-08-01 23:39:39 +0100

    Add 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module to calculate the perimeter of an island in a grid.
+"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island in a given grid.
+    """
+    perimeter = 0
+    if not isinstance(grid, list):
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
