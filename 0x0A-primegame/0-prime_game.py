764ff19d504500bf32afd166829f38c62a3b8aec
dev-soufiane <dev.sofyane@gmail.com>
2024-08-08 00:49:12 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,54 +2,60 @@
 # Prime game winner determination
 
 
+def is_prime_numbers(n):
+    """
+    Returns prime numbers between 1 and n using the sieve of Eratosthenes.
+    Args:
+        n (int): The upper limit to generate prime numbers.
+    Returns:
+        list of int: Prime numbers between 1 and n.
+    """
+    prime_numbs = []  # List to store the prime numbers
+    # Boolean list to mark all numbers as prime (Sieve of Eratosthenes)
+    sieve = [True] * (n + 1)
+
+    # Iterate over the numbers from 2 to n
+    for num in range(2, n + 1):
+        if sieve[num]:  # If number is marked as prime, adds to the list
+            prime_numbs.append(num)
+            # Mark all multiples of the current number as non-prime
+            for i in range(num, n + 1, num):
+                sieve[i] = False
+    return prime_numbs  # Return the list of prime numbers
+
+
 def isWinner(x, nums):
-    """Determine who won the most rounds."""
-
-    def sieve(n):
-        """Generate list of primes up to n using Sieve of Eratosthenes."""
-        is_prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    def game_winner(n):
-        """Determine the winner of the game with n."""
-        primes = sieve(n)
-        remaining = set(range(1, n + 1))
-        turn = 0  # Maria starts (0), Ben is (1)
-
-        while primes:
-            p = primes.pop(0)
-            if p > n:
-                break
-            if p in remaining:
-                # Remove multiples of p
-                for multiple in range(p, n + 1, p):
-                    remaining.discard(multiple)
-                turn = 1 - turn  # Switch turn
-
-        return "Maria" if turn == 1 else "Ben"
-
-    if x == 0:
+    """
+    Determines the winner of the prime game given the rounds and numbers.
+    Args:
+        x (int): Number of rounds to play.
+        nums (list of int): A list of the numbers rolled in each round.
+    Returns:
+        str: The name of the player that won the most rounds.
+        None: If no winner can be determined.
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
 
-    win_count = {"Maria": 0, "Ben": 0}
-    for n in nums:
-        winner = game_winner(n)
-        if winner:
-            win_count[winner] += 1
+    # Number of rounds Maria & Ben has won
+    maria_wins_count = 0
+    ben_wins_count = 0
 
-    if win_count["Maria"] > win_count["Ben"]:
+    # Iterate over the list of numbers
+    for n in range(x):
+        # Get the list of prime numbers for the number `nums[n]`
+        prime_numbs = is_prime_numbers(nums[n])
+        # If the length of primes is odd, Maria wins the round
+        if len(prime_numbs) % 2 != 0:
+            maria_wins_count += 1
+        else:
+            # Otherwise, Ben wins the round.
+            ben_wins_count += 1
+
+    # Return the name of the player with the most wins.
+    if maria_wins_count > ben_wins_count:
         return "Maria"
-    elif win_count["Ben"] > win_count["Maria"]:
+    elif ben_wins_count > maria_wins_count:
         return "Ben"
     else:
         return None
-
-
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
